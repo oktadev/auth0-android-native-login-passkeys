9c2e15ae814726662a07085e6162be58358600da
Carla Urrea Stabile <2745491+carlastabile@users.noreply.github.com>
2024-10-03 11:19:00 +0200

    add passkey icon

--- app/src/main/java/com/example/nativepasskeys/MainActivity.kt
+++ app/src/main/java/com/example/nativepasskeys/MainActivity.kt
@@ -26,31 +26,6 @@ class MainActivity : ComponentActivity() {
             getString(R.string.auth0_domain)
         )
 
-        setContent {
-            NativePasskeysTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NativePasskeysTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
